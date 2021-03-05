e09dbcc7790eb6f94153ce8ca573e812a47c5160
Michael Tsukerman <miketsukerman@gmail.com>
2021-03-05 11:20:07 +0100

    Simple event loop implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main(int argc, char **argv)
 {
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
     return 0;
 }
