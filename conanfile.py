c88573322668a79006d51627a35b5521c9b2c3a4
Michael Tsukerman <miketsukerman@gmail.com>
2021-05-30 19:00:19 +0200

    conan: fix recipe
    Add name and version

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,24 @@
 from conans import ConanFile, CMake
 
 class GeodConan(ConanFile):
+   name = "geod"
+   version = "0.0.1"
    settings = "os", "compiler", "build_type", "arch"
    requires = "cppzmq/4.7.1", "spdlog/[>=1.4.1]", "CLI11/1.9.1@cliutils/stable"
    generators = "cmake", "gcc", "txt", "cmake_find_package"
    default_options = {}
-   
-   def imports(self):
-      self.copy("*.dll", dst="bin", src="bin") # From bin to bin
-      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
-   
+   exports_sources = "*"
+
+   @property
+   def _source_subfolder(self):
+      return "source_subfolder"
+     
    def build(self):
       cmake = CMake(self)
       cmake.configure()
       cmake.build()   
+
+   def imports(self):
+      self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+   
