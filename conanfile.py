0af888b48cd74f642ad109d7849bd045fcd5c87a
Michael Tsukerman <miketsukerman@gmail.com>
2021-06-06 21:18:56 +0200

    systemd: service install path
    Add cmake option for systemd service file path

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,30 @@
 from conans import ConanFile, CMake
+import os
 
 class GeodConan(ConanFile):
    name = "geod"
    version = "0.0.1"
    settings = "os", "compiler", "build_type", "arch"
-   requires = "cppzmq/4.7.1", "spdlog/[>=1.4.1]", "CLI11/1.9.1@cliutils/stable"
+   requires = "adcr/0.0.1@zdev/testing", "cppzmq/4.7.1"
    generators = "cmake", "gcc", "txt", "cmake_find_package"
    default_options = {}
-   exports_sources = "*"
 
-   @property
-   def _source_subfolder(self):
-      return "source_subfolder"
+   scm = {
+      "type" : "git",
+      "subfolder" : ".",
+      "url" : "https://github.com/zuckerman-dev/geod.git",
+      "revision" : "auto", 
+      "submodule" : "recursive"
+   }
+   no_copy_source = True
+   keep_imports = True
      
    def build(self):
       cmake = CMake(self)
+      cmake.definitions["SYSTEMD_UNIT_PATH"] = os.path.join(self.package_folder,"lib/systemd/system")
       cmake.configure()
       cmake.build()   
+      cmake.install()
 
    def imports(self):
       self.copy("*.dll", dst="bin", src="bin") # From bin to bin
